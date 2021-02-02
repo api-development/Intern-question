e48bc117a3c6987fd3a1f10894189b3392edeed8
Apiphoom Chuenchompoo <apiphoom23@gmail.com>
2021-02-02 14:59:29 +0700

    Update problem-2.py

--- problem-2.py
+++ problem-2.py
@@ -1,6 +1,7 @@
 # Print rows rows of the Pascal's triangle but make the first number start. The Pascal's triangle is a triangle which has one 1 on the top, 2 ones below that, and for the next few rows are the sum of the two numbers above it. There should not be spaces before the first number but put spaces in-between each number.
 
-
+#Line 1: An integer rows for the amount of rows to output
+#Line 2: An integer start for the first number.
 
 #5
 #5 5
